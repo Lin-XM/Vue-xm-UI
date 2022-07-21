ad427797f039b937add2a6f5c65f90fad94535f4
xiaoming <2604863701@qq.com>
2022-07-21 18:57:52 +0800

    button的样式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Button></Button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import Button from './components/Button'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Button
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    * {
+        margin: 0;
+        padding: 0;
+        -webkit-box-sizing: border-box;
+        -moz-box-sizing: border-box;
+        box-sizing: border-box;
+    }
+
+    :root {
+        --button-height: 32px;
+        --font-size: 14px;
+        --button-active-bg: #eee;
+        --button-bg: white;
+        --border-radius: 4px;
+        --color: #999;
+        --border-color: #999;
+        --border-color-hover: #666;
+    }
+
+
+
+    #app {
+        margin: 20px;
+    }
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <button class="g-button" >
+            <img class="icon" src="../assets/齿轮.svg" alt="">
+            按钮
+        </button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Button"
+    }
+</script>
+
+<style scoped>
+    .icon {
+        width: 1em; height: 1em;
+        vertical-align: -0.15em;
+        fill: currentColor;
+        overflow: hidden;
+    }
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background-color: var(--button-bg);
+
+    }
+
+    .g-button:hover {
+        border-color: var(--border-color);
+    }
+
+    .g-button:active {
+        background-color: var(--button-active-bg);
+    }
+
+    .g-button:focus {
+        outline: none;
+    }
+</style>
